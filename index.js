eed01fac12675c472dce7b8e12298d9fab2110ca
Paolo Rufrano <paolo.r@afrihost.com>
2020-03-27 16:33:38 +0200

    setup

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const request = require('request')
+const app = express()
+const port = 3000
+const fs = require('fs')
+const baseUrl = 'https://covid19.mathdro.id/api'
+
+app.get('/ping', (req, res) => res.send('pong'))
+
+app.get('/covid/:iso3/:segment', (req, res) => 
+{  
+    let segment = req.params.segment
+    console.log(segment)
+    let endpoint = `${baseUrl}/countries/${req.params.iso3}/${segment}`
+
+    request({ url: endpoint }, (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                res.send('no data returned')
+                return
+            }
+
+            let data = body ? JSON.parse(body)[0] : {}
+
+            switch (segment) {
+                case 'recovered':
+                    res.send(`${data.recovered} recovered in ${data.combinedKey}`)
+                    break;
+
+                case 'deaths':
+                    res.send(`${data.deaths} deaths in ${data.combinedKey}`)
+            
+                default:
+                    res.send(`${data.confirmed} confirmed cases in ${data.combinedKey}`)
+                    break;
+            }
+        }
+    )
+})
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
