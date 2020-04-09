5654b5a77013fb5e574709c89050dc12154b8847
Paolo Rufrano <rufranop@gmail.com>
2020-04-09 10:15:16 +0200

    update covid endpoint

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express')
 const request = require('request')
 const app = express()
 const port = 3000
-const baseUrl = 'https://covid19.mathdro.id/api'
+const baseUrl = 'https://corona.lmao.ninja'
 
 app.get('/ping', (req, res) => res.send('pong'))
 
-app.get('/covid/:iso3/:segment', (req, res) => 
-{  
+app.get('/covid/:iso3/:segment', (req, res) => {
+
     let segment = req.params.segment
-    console.log(segment)
-    let endpoint = `${baseUrl}/countries/${req.params.iso3}/${segment}`
+    let endpoint = `${baseUrl}/countries/${req.params.iso3}`
 
     request({ url: endpoint }, (error, response, body) => {
             if (error || response.statusCode !== 200) {
@@ -18,18 +17,18 @@ app.get('/covid/:iso3/:segment', (req, res) =>
                 return
             }
 
-            let data = body ? JSON.parse(body)[0] : {}
+            let data = JSON.parse(body)
 
             switch (segment) {
                 case 'recovered':
-                    res.send(`${data.recovered} recovered in ${data.combinedKey}`)
+                    res.send(`${data.recovered} recovered in ${data.country}`)
                     break;
 
                 case 'deaths':
-                    res.send(`${data.deaths} deaths in ${data.combinedKey}`)
+                    res.send(`${data.deaths} deaths in ${data.country}`)
             
                 default:
-                    res.send(`${data.confirmed} confirmed cases in ${data.combinedKey}`)
+                    res.send(`${data.cases} confirmed cases in ${data.country}`)
                     break;
             }
         }
